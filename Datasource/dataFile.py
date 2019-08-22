65297e2180cc9869e56ddc23ee531e18324a6987
Jean Carlos de Oliveira <jeancarlosdeoliveira@outlook.com>
2019-08-22 09:25:35 -0300

    Finalização da primeira entrega
    Finalização da primeira entrega

--- Datasource/dataFile.py
+++ Datasource/dataFile.py
@@ -6,35 +6,54 @@ import os
 def dataReadMatrix(name, id):
     fileName = "Datasource\\Datasets\\" + name + id + ".txt"
 
-    with open(fileName, 'rb') as file:
-        container = [float(field) for field in file.readline().split()]
-        container.pop(0)
+    try:
+        with open(fileName, 'rb') as file:
+            container = [float(field) for field in file.readline().split()]
+            container.pop(0)
 
-        'data = [ [ float(j) for j in i.split() ] for i in file]'
-        if(name == "rect"):
-            data = [[float(j)*100 for j in i.split()] for i in file]
-        if(name == "square"):
-            data = [[float(i)*100, float(i)*100] for i in file]
+            if(name == "rect"):
+                data = [[float(j) for j in i.split()] for i in file]
+            if(name == "square"):
+                data = [[float(i), float(i)] for i in file]
+    except:
+        print("\nArquivo inválido!\n")
+        return [0,0]
 
-        return container, data
+    return container, data
 
 ' REALIZA A ESCRITA DOS ARQUIVOS EM ARQUIVO - NADA AINDA IMPLEMENTADO'
 def dataWrite(nome, id, container, data):
     filename = 'Datasource\\' + nome + id +'_altered.txt'
-    file = open(filename, 'a+')
+    'file = open(filename, "a+")'
+    file = open(filename, 'w')
 
-    [file.writelines(str("%.2f" %i)) for i in container]
+    'GRAVA OS VALORES DO CONTAINER'
+    [file.writelines(str("%.2f" %i) + ";") for i in container]
     file.writelines("\n")
 
+    'GRAVA OS VALORES DAS PEÇAS, SEJA ELA UM RETANGULO OU UM QUADRADO'
     [[[file.writelines(str("%.2f" %j) + ";") for j in i],file.writelines("\n")] for i in data]
 
-    print('\nWrited!!!\n')
-    print(os.path.abspath(os.curdir) + "/" + filename)
+    'IMPRIME UMA MENSAGEM E O LOCAL ONDE FOI SALVO O ARQUIVO'
+    print('\nArquivo salvo!')
+    print(os.path.abspath(os.curdir) + "/" + filename+"\n")
     file.close()
 
-    'IMPRIME OS VALORES MODIFICADOS'
-    print("\nRaio dos containers: ", end = '')
+' REALIZA A IMPRESSÃO DOS DADOS NA TELA '
+def printMatriz(title, container, data):
+    'IMPRIME O TÍTULO'
+    print("\n" + title, end = '')
+
+    'IMPRIME OS CONTAINERS'
+    'PARA CADA I EM CONTAINER, ELE FAZ UM CAST PARA FLOAT E DEPOIS PARA STRING'
     [ print(str(float(i))+ ' ', end = '') for i in container]
     print("\n")
 
+    'IMPRIME OS DADOS'
+    'PARA CARA I EM DATA FAZ UM CAST PARA STRING COM 2 CASAS DECIMAIS DO TIPO FLOAT'
+    'ESTE É O NOVO FORMATO DE DADOS. TODOS OS QUADRADOS E RETANGULOS POSSUEM ALTURA E LARGURA'
     [ print( str("%.2f " %i[0]) + str("%.2f " %i[1]) ) for i in data]
+    print("\n")
+
+def alterMatriz(alter, data):
+    return [[j*alter for j in i] for i in data]
--- main.py
+++ main.py
@@ -12,16 +12,81 @@ from Datasource import dataFile as dt
 'APENAS PARA ORGANIZAÇÂO'
 fileType = {0:"square", 1:"rect"}
 
-'CHAMADA EXTERNA'
+'''
+    Essa variável define como o codigo vai rodar.
+    Se ela permanecer em 0, vai quer dizer que o comando veio externo e vai rodar automaticamente
+    Se ela mudar para 1, vai exibir um menu e continuar em loop ate encerrar
+'''
+runType = 0
+
+'CHAMADA EXTERNA - RECEBENDO OS ARGUMENTOS ENVIADOS VIA TERMINAL'
 if __name__ == "__main__":
     if len(sys.argv) == 1:
-        print("Sem dados de entrada. Saindo....")
-        exit(1)
+        print("Sem dados de entrada. Saindo....\nAbrindo o menu para seleção:")
+        runType = 1
+
+if runType == 0:
     Type    = fileType[int(sys.argv[1])]
     number  =  str(sys.argv[2])
 
-'REALIZA A LEITURA DO ARQUIVO'
-container, data = dt.dataReadMatrix( Type, number )
+    'REALIZA A LEITURA DO ARQUIVO'
+    container, data = dt.dataReadMatrix( Type, number )
 
-'REALIZA A ESCRITA DO ARQUIVO MODIFICADO'
-dt.dataWrite(Type, number, container, data)
+    'REALIZA A ESCRITA DO ARQUIVO MODIFICADO'
+    dt.dataWrite(Type + "fromArgv", number, container, data)
+
+elif runType == 1:
+    print("#### CUTTING STOCK (PROBLEMA DE CORTE DE ESTOQUE) ####\n")
+
+    'LOOPING INFINITO ATÉ QUE O USUÁRIO SELECIONE PARA SAIR'
+    while(1):
+        'MENU INFORMATIVO'
+        print("\nSelecione:\n\t1: Ler um arquivo\n\t2: Alterar um arquivo\n\t3: Escrever um arquivo\n\t4: Imprimir um arquivo\n\t0: Saída")
+        select = input(">>>: ")
+
+        'FECHAR CASO RECEBA 0'
+        if select == '0':
+            exit()
+
+        'CASE PARA LEITURA'
+        if select == "1":
+            try:
+                name = input("\nDigite 0 para Quadrado ou 1 para Retangulo: ")
+                name = fileType[int(name)]
+
+                id = input("Qual o número do dataset: ")
+
+                'REALIZA A LEITURA DO ARQUIVO'
+                container, data = dt.dataReadMatrix(name, id)
+
+            except:
+                print("Dataset inválido....")
+
+        'VALIDA SE JA FOI REALIZADAA LEITURA'
+        try:
+            if len(data) >= 0:
+                'ALTERAR OS DADOS'
+                if select == "2":
+                    try:
+                        value = input("\nDigite um valor a multiplicar os itens do arquivo: ")
+                        data = dt.alterMatriz(int(value), data)
+                    except:
+                        print("\nMultiplicação inválida. Talvez tenha escrito uma letra.")
+
+                'REALIZA A ESCRITA EM ARQUIVO DO ARQUIVO MODIFICADO'
+                if select == "3":
+                    dt.dataWrite(Type, number, container, data)
+
+                'REALIZA A IMPRESSÃO DO ARQUIVO MODIFICADO'
+                if select == "4":
+                    dt.printMatriz('Raio dos containers: ', container, data)
+
+            else:
+                print("\nDados ainda não carregados\n")
+
+        except:
+                print("\nOpção inválida. Dados ainda não carregados\n")
+
+
+
+print("Encerrado...")
